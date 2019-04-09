bca0f59495d057386a10955f65364707cae99d0c
morisho <morisho.ongala@gmail.com>
2019-04-09 10:45:52 +0200

    first commit

---
+++ admin/admin/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MyOwnCustomMaterialModule} from '../components/components.module';
+import { LoginComponent } from './login/login.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MyOwnCustomMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
