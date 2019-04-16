c7bd89381cbbc3bc058c3611b45d1bc8d63da167
morisho <morisho.ongala@gmail.com>
2019-04-16 08:43:50 +0300

    login craft done!

--- admin/admin/src/app/app-routing.module.ts
+++ admin/admin/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
 
--- admin/admin/src/app/app.module.ts
+++ admin/admin/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MyOwnCustomMaterialModule} from '../components/components.module';
+// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import {MyOwnCustomMaterialModule} from '../components/components.module';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MyOwnCustomMaterialModule
+    // BrowserAnimationsModule,
+    // MyOwnCustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
